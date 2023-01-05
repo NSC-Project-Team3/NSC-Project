7e3f85cceb1d7b2bb7773c9b12cfc4412507589a
Chutiendat26092003 <chutiendat148@gmail.com>
2023-01-05 11:13:12 +0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using NSC_project.Data;
+using NSC_project.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<NSC_projectContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("NSC_projectContext") ?? throw new InvalidOperationException("Connection string 'NSC_projectContext' not found.")));
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+
+    SeedData.Initialize(services);
+}
+
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
